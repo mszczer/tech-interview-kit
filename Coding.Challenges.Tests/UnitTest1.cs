35e2cd2953e9e18240c7b37adbfa44c7abb8ac2f
Maciej <maciej@szczerkowski.com>
2023-07-31 10:24:27 +0200

    Initial project setup

---
+++ Coding.Challenges.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Coding.Challenges;
+using NUnit.Framework;
+
+namespace Coding.Challenges.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
