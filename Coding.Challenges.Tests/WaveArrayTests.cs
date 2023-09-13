75cb601f29d3327dc8d92a13c30452ebb90e50de
Maciej <maciej@szczerkowski.com>
2023-09-13 13:47:59 +0200

    Wave Array

---
+++ Coding.Challenges.Tests/WaveArrayTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Coding.Challenges.Tests;
+
+[TestFixture]
+public class WaveArrayTests
+{
+    [Test]
+    [TestCase(new[] { 4, 2, 9, 1, 21, 43, 24 }, new[] { 2, 1, 9, 4, 24, 21, 43 })]
+    public void GetWaveArray_ReturnsLexicographicallySmallestWaveArray(int[] input, int[] expected)
+    {
+        Assert.That(WaveArray.GetWaveArray(input), Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCase(new[] { 5, 2, 9, 3, 2 })]
+    [TestCase(new[] { 3, 2, 9, 6, 4, 1 })]
+    [TestCase(new[] { 4, 2, 9, 1, 21, 43, 24 })]
+    public void GetWaveArray_ReturnsWaveArray(int[] input)
+    {
+        var testArr = WaveArray.GetWaveArray(input);
+
+        var checkResult = true;
+
+        for (var i = 0; i < testArr.Length - 1; i += 2)
+            if ((i + 1) % 2 == 0)
+                if (testArr[i] > testArr[i - 1] || testArr[i] > testArr[i + 1])
+                {
+                    checkResult = false;
+                    break;
+                }
+
+        Assert.That(checkResult, Is.True);
+    }
+
+    [Test]
+    [TestCase(new[] { 5, 2, 9, 3, 2 })]
+    [TestCase(new[] { 3, 2, 9, 6, 4, 1 })]
+    [TestCase(new[] { 4, 2, 9, 1, 21, 43, 24 })]
+    public void GetWaveArrayComparingNeighbors_ReturnsWaveArray(int[] input)
+    {
+        var testArr = WaveArray.GetWaveArrayComparingNeighbors(input);
+
+        var checkResult = true;
+
+        for (var i = 0; i < testArr.Length - 1; i += 2)
+            if ((i + 1) % 2 == 0)
+                if (testArr[i] > testArr[i - 1] || testArr[i] > testArr[i + 1])
+                {
+                    checkResult = false;
+                    break;
+                }
+
+        Assert.That(checkResult, Is.True);
+    }
+}
---
+++ Coding.Challenges/WaveArray.cs
@@ -0,0 +1,37 @@
+﻿namespace Coding.Challenges;
+
+/*
+ * Difficulty: Easy
+ * Problem:
+ *  Given an unsorted array of integers( arr ) of length n , write a program to sort it in wave form.
+ *      The array elements in the resultant array must be such that arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4]
+ *      If there are multiple sorted orders in wave form, return the one which is lexicographically smallest.
+ *      The array may contain duplicates.
+ */
+
+public abstract class WaveArray
+{
+    public static int[] GetWaveArray(int[] arr)
+    {
+        Array.Sort(arr);
+
+        for (var i = 1; i < arr.Length; i++)
+            if ((i + 1) % 2 == 0)
+                (arr[i], arr[i - 1]) = (arr[i - 1], arr[i]);
+
+        return arr;
+    }
+
+    public static int[] GetWaveArrayComparingNeighbors(int[] arr)
+    {
+        for (var i = 0; i < arr.Length; i += 2)             // traverse all even positioned elements of the input array
+        {
+            if (i > 0 && arr[i - 1] > arr[i])               // If current even index element is smaller than previous 
+                (arr[i], arr[i - 1]) = (arr[i - 1], arr[i]);
+            if (i < arr.Length - 1 && arr[i] < arr[i + 1])  // If current even index element is smaller than next 
+                (arr[i], arr[i + 1]) = (arr[i + 1], arr[i]);
+        }
+
+        return arr;
+    }
+}
