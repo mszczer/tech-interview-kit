ffeb092434f8d54c2bfb5fc1217a4d42994ce8fc
Maciej <maciej@szczerkowski.com>
2023-10-05 16:03:13 +0200

    Move zeroes to the end

---
+++ Coding.Challenges.Tests/ZeroesToEndTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Coding.Challenges.Tests;
+
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class ZeroesToEndTests
+{
+    [Test]
+    [TestCase(new[] { 1, 8, 0, 2, 0, 1, 13, 0 }, new[] { 1, 8, 2, 1, 13, 0, 0, 0 })]
+    [TestCase(new[] { 0, 0, 0, 23, 2 }, new[] { 23, 2, 0, 0, 0 })]
+    public void MoveZeroesToEnd_ReturnsArrayWithZeroesAtEnd(int[] input, int[] expected)
+    {
+        Assert.That(ZeroesToEnd.MoveZeroesToEnd(input), Is.EqualTo(expected));
+    }
+
+}
---
+++ Coding.Challenges/ZeroesToEnd.cs
@@ -0,0 +1,27 @@
+﻿namespace Coding.Challenges;
+
+/*
+ * Difficulty: Easy
+ * Problem:
+ *  Given an array arr[] of n elements filled with several integers, some of them being zeroes,
+ *  move all the zeroes to the end.
+ *      The relative order of non-zero integers needs to remain the same as in the original array.
+ *      You must do this in-place without making a copy of the array.
+ *      Minimize the total number of operations.
+ */
+public abstract class ZeroesToEnd
+{
+    public static int[] MoveZeroesToEnd(int[] arr)
+    {
+        var nonZeroIdx = 0;
+
+        for (var i = 0; i < arr.Length; i++)
+            if (arr[i] != 0)
+            {
+                (arr[i], arr[nonZeroIdx]) = (arr[nonZeroIdx], arr[i]);
+                nonZeroIdx++;
+            }
+
+        return arr;
+    }
+}
