1c08e020509dc05b4b5bcad78f4387a5bd9dc1d6
mszczer <Softw@r3Hub>
2025-09-03 12:08:03 +0200

    Repository sync

---
+++ Coding.Challenges/Easy/PathSumInBinaryTree.cs
@@ -0,0 +1,12 @@
+﻿namespace Coding.Challenges.Easy;
+/*
+ * Difficulty: Easy
+ * Problem:
+ *  Given the root node of a binary tree and a sum, write a program
+ *  to determine if there exists a path in the tree from root-to-leaf
+ *  such that the sum of all the values along the path equals the given sum.
+ */
+
+public abstract class PathSumInBinaryTree
+{
+}
