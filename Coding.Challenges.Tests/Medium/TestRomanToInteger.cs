824c9513ad9321f719c72f36735e842fcf7d5c1f
mszczer <Softw@r3Hub>
2026-03-03 16:16:26 +0100

    Add Roman numeral to integer converter and tests
    Implemented RomanToInteger class with ConvertRomanToInt method to parse Roman numerals to integers.
    Added NUnit tests for various cases to verify correctness.

---
+++ Coding.Challenges.Tests/Medium/TestRomanToInteger.cs
@@ -0,0 +1,18 @@
+﻿using Coding.Challenges.Medium;
+
+namespace Coding.Challenges.Tests.Medium;
+
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class TestRomanToInteger
+{
+    [TestCase("VII", 7, TestName = "ConvertRomanToInt_VII_Returns7")]
+    [TestCase("IV", 4, TestName = "ConvertRomanToInt_IV_Returns4")]
+    [TestCase("XC", 90, TestName = "ConvertRomanToInt_XC_Returns90")]
+    [TestCase("XVII", 17, TestName = "ConvertRomanToInt_XVII_Returns17")]
+    public void ConvertRomanToInt_Examples_ReturnExpected(string roman, int expected)
+    {
+        var actual = RomanToInteger.ConvertRomanToInt(roman);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
---
+++ Coding.Challenges/Medium/RomanToInteger.cs
@@ -0,0 +1,55 @@
+﻿namespace Coding.Challenges.Medium;
+
+/*
+ * Difficulty: Medium
+ * Problem:
+ *  Given a string s representing a roman numeral. Convert s into an integer.
+ * Note:
+ *  s is guaranteed to be within the range from 1 to 3999.
+ */
+
+public class RomanToInteger
+{
+    private static readonly Dictionary<char, int> RomanMap = new Dictionary<char, int>
+    {
+        { 'I', 1 },
+        { 'V', 5 },
+        { 'X', 10 },
+        { 'L', 50 },
+        { 'C', 100 },
+        { 'D', 500 },
+        { 'M', 1000 }
+    };
+
+    public static int ConvertRomanToInt(string roman)
+    {
+        if (string.IsNullOrWhiteSpace(roman))
+            throw new ArgumentException("Input must be a non-empty Roman numeral.", nameof(roman));
+
+        var result = 0;
+        for (var i = roman.Length - 1; i >= 0; i--)
+        {
+            var currentChar = char.ToUpperInvariant(roman[i]);
+
+            if (!RomanMap.TryGetValue(currentChar, out var currentValue))
+                throw new ArgumentException($"Invalid Roman numeral character '{roman[i]}'.", nameof(roman));
+
+            if (i < roman.Length - 1)
+            {
+                var nextChar = char.ToUpperInvariant(roman[i + 1]);
+                var nextValue = RomanMap[nextChar];
+
+                if (currentValue < nextValue)
+                    result -= currentValue;
+                else
+                    result += currentValue;
+            }
+            else
+            {
+                result += currentValue;
+            }
+        }
+
+        return result;
+    }
+}
