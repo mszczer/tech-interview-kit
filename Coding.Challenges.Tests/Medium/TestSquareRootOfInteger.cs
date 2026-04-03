c07aed517f74ee817958d32253a27bbbd1485105
mszczer <maciej@szczerkowski.com>
2026-04-03 16:05:19 +0200

    Add SquareRootOfInteger class with tests for sqrt and factors
    Implemented LinearSearchSquareRoot, BinarySearchSquareRoot, and GetPrimeFactors methods in the new SquareRootOfInteger class.
    Added comprehensive NUnit tests covering edge cases, large numbers, and prime factorization to ensure correctness of all methods.

---
+++ Coding.Challenges.Tests/Medium/TestSquareRootOfInteger.cs
@@ -0,0 +1,41 @@
+﻿using Coding.Challenges.Medium;
+
+namespace Coding.Challenges.Tests.Medium;
+
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class TestSquareRootOfInteger
+{
+    [TestCase(0, 0)]
+    [TestCase(1, 1)]
+    [TestCase(4, 2)]
+    [TestCase(11, 3)]
+    public void LinearSearchSquareRoot_ReturnsExpectedResult(int input, int expectedResult)
+    {
+        var result = SquareRootOfInteger.LinearSearchSquareRoot(input);
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [TestCase(0, 0)]
+    [TestCase(1, 1)]
+    [TestCase(4, 2)]
+    [TestCase(11, 3)]
+    [TestCase(2147395600, 46340)]
+    [TestCase(2147483647, 46340)]
+    public void BinarySearchSquareRoot_ReturnsExpectedResult(int input, int expectedResult)
+    {
+        var result = SquareRootOfInteger.BinarySearchSquareRoot(input);
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [TestCase(2, new[] { 2 })]
+    [TestCase(12, new[] { 2, 2, 3 })]
+    [TestCase(17, new[] { 17 })]
+    [TestCase(60, new[] { 2, 2, 3, 5 })]
+    [TestCase(100, new[] { 2, 2, 5, 5 })]
+    public void GetPrimeFactors_ReturnsExpectedResult(int input, int[] expectedFactors)
+    {
+        var result = SquareRootOfInteger.GetPrimeFactors(input);
+        Assert.That(result, Is.EqualTo(expectedFactors));
+    }
+}
---
+++ Coding.Challenges/Medium/SquareRootOfInteger.cs
@@ -0,0 +1,60 @@
+﻿namespace Coding.Challenges.Medium;
+
+/*
+ * Difficulty: Medium
+ * Problem:
+ *  Write a program to compute and return the square root of an integer num.
+ *  Where num is guaranteed to be a non-negative integer.
+ *  If num is not a perfect square, then return floor(√num)
+ */
+
+public class SquareRootOfInteger
+{
+    public static int LinearSearchSquareRoot(int num)
+    {
+        if (num is 0 or 1) return num;
+        var squareRoot = 2;
+        while (squareRoot * squareRoot <= num) squareRoot++;
+
+        return squareRoot - 1;
+    }
+
+    public static int BinarySearchSquareRoot(int num)
+    {
+        if (num is 0 or 1) return num;
+        var left = 2;
+        var right = num / 2;
+        while (left <= right)
+        {
+            var middle = (left + right) / 2;
+            var middleSquared = (long)middle * middle;
+            if (middleSquared == num) return middle;
+            if (middleSquared < num) left = middle + 1;
+            else right = middle - 1;
+        }
+
+        return right;
+    }
+
+    public static List<int> GetPrimeFactors(int num)
+    {
+        var primeFactors = new List<int>();
+
+        if (num < 0)
+        {
+            primeFactors.Add(-1);
+            num = Math.Abs(num);
+        }
+        
+        for (var i = 2; i <= num / i; i++)
+            while (num % i == 0)
+            {
+                primeFactors.Add(i);
+                num /= i;
+            }
+
+        if (num > 1) 
+            primeFactors.Add(num);
+        return primeFactors;
+    }
+}
