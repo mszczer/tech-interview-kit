1c4427a5183d0b9d3f6a4db65492b809461d9c35
mszczer <maciej@szczerkowski.com>
2026-09-03 15:37:57 +0200

    Add ClosestPalindrome class and related tests
    Added ClosestPalindrome with FindClosestPalindrome method and IsPalindrome helper. Introduced TestClosestPalindrome for comprehensive testing. Updated Coding.Challenges.csproj to remove obsolete Hard folder ItemGroup.

---
+++ Coding.Challenges.Tests/Hard/TestClosestPalindrome.cs
@@ -0,0 +1,26 @@
+﻿using Coding.Challenges.Hard;
+
+namespace Coding.Challenges.Tests.Hard;
+
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class TestClosestPalindrome
+{
+    [Test]
+    [TestCase("88", "77")]
+    [TestCase("1", "0")]
+    [TestCase("9", "8")]
+    [TestCase("10", "9")]
+    [TestCase("11", "9")]
+    [TestCase("121", "111")]
+    [TestCase("123", "121")]
+    [TestCase("1000", "999")]
+    [TestCase("999", "1001")]
+    [TestCase("1283", "1331")]
+    [TestCase("100000", "99999")]
+    public void FindClosestPalindrome_ReturnsExpectedResult(string number, string expectedResult)
+    {
+        var result = ClosestPalindrome.FindClosestPalindrome(number);
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
---
+++ Coding.Challenges/Hard/ClosestPalindrome.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Difficulty: Hard
+ * Problem:
+ *  Given an integer num, write a program to find the closest palindrome to integer num. (excluding num).
+ *  The output need not be larger than num. It can be smaller or larger than num.
+ *  Problem Note:
+ *      An integer is said to be a palindrome if it remains the same when its digits are reversed.
+ *      'Closest' means that the absolute difference between two integers should be minimum.
+ *      num is a positive integer represented by a string, whose length will not exceed 15 digits.
+ *      If two palindromic integers are closest to num and there is a tie between them, then return the smaller one as output.
+ */
+
+namespace Coding.Challenges.Hard;
+
+public static class ClosestPalindrome
+{
+    public static string FindClosestPalindrome(string num)
+    {
+        ArgumentNullException.ThrowIfNull(num);
+
+        var originalNum = long.Parse(num);
+        var closestPalindrome = long.MinValue;
+
+        for (long i = 1;; i++)
+        {
+            // Check lower palindrome
+            var lowerCandidate = originalNum - i;
+            if (lowerCandidate > 0 && IsPalindrome(lowerCandidate))
+            {
+                closestPalindrome = lowerCandidate;
+                break;
+            }
+
+            // Check upper palindrome
+            var upperCandidate = originalNum + i;
+            if (IsPalindrome(upperCandidate))
+            {
+                closestPalindrome = upperCandidate;
+                break;
+            }
+        }
+
+        return closestPalindrome.ToString();
+    }
+
+    private static bool IsPalindrome(long num)
+    {
+        var str = num.ToString();
+        for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            if (str[i] != str[j])
+                return false;
+
+        return true;
+    }
+}
